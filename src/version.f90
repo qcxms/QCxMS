65ad8854281b61f1f974935dfd2a04c166ad6945
JayTheDog <jeroen.koopman@thch.uni-bonn.de>
2022-02-15 18:44:23 +0100

    updated version

--- src/version.f90
+++ src/version.f90
@@ -7,14 +7,14 @@ contains
      integer,intent(in) :: i
      character(len=:), allocatable  :: line
 
-     line ='Feb 14 16:30:00 CEST 2021 '
+     line ='Feb 15 18:30:00 CEST 2021 '
 
      if (i.eq.0)then
-       write(*,' (22x,''*'',18x,''V5.2.0- REBUILD'',18x,'' *'')'  )
+       write(*,' (22x,''*'',18x,''V5.2.0'',18x,'' *'')'  )
      endif
      if (i.eq.1)  write(*,' (22x,''*        '',(a)''         *'')'     ) line
-     if (i.eq.2)  write(*,' (22x,''--- QCxMS V5.2.0- REBUILD'',(a)'' ---'')') line
-     if (i.eq.33) write(33,'(22x,''--- QCxMS V5.2.0- REBUILD'',(a)'' ---'')') line
+     if (i.eq.2)  write(*,' (22x,''--- QCxMS V5.2.0'',(a)'' ---'')') line
+     if (i.eq.33) write(33,'(22x,''--- QCxMS V5.2.0'',(a)'' ---'')') line
 
    end subroutine version
 
